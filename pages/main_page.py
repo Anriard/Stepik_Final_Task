ba2c2c39443da2be4abe3bcc5c97a607e4028500
Anriard <alex.anriard@gmail.com>
2021-08-02 13:55:05 +0300

    redirect_to_login_page_added

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
+from .login_page import LoginPage
 
 class MainPage(BasePage):
     def go_to_login_page(self):
@@ -8,3 +9,13 @@ class MainPage(BasePage):
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+
+
+
+    def test_guest_can_go_to_login_page(browser):
+        link = "http://selenium1py.pythonanywhere.com"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
